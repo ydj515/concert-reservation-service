adebe67b217140e8dc50a1e59e1682b905f5e98b
ydj515 <ydj515@hanmail.net>
2025-01-05 01:09:46 +0900

    feat: clock config 추가

---
+++ src/test/kotlin/io/hhplus/concertreservationservice/config/ClockConfigTest.kt
@@ -0,0 +1,20 @@
+package io.hhplus.concertreservationservice.config
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Import
+import java.time.Clock
+import java.time.ZoneId
+
+@Import(ClockConfig::class)
+class ClockConfigTest(
+    @Autowired val clock: Clock,
+) : FunSpec({
+        test("clock 설정을 테스트 한다.") {
+            val currentInstant = clock.instant()
+            val zonedDateTime = currentInstant.atZone(ZoneId.of("Asia/Seoul"))
+
+            zonedDateTime.zone.id shouldBe "Asia/Seoul" // 테스트 확인
+        }
+    })
