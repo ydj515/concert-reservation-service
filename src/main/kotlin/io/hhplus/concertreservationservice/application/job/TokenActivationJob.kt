187d866233d07772f10ffb616a0bdc6c1956962a
ydj515 <ydj515@hanmail.net>
2025-01-10 04:36:19 +0900

    feat: 토큰 활성화/비활성화, 자동예약 취소 스케쥴러 추가

---
+++ src/main/kotlin/io/hhplus/concertreservationservice/application/job/TokenActivationJob.kt
@@ -0,0 +1,41 @@
+package io.hhplus.concertreservationservice.application.job
+
+import io.hhplus.concertreservationservice.domain.token.ReservationTokenConstants.MAX_TOKEN_COUNT
+import io.hhplus.concertreservationservice.domain.token.TokenStatus
+import io.hhplus.concertreservationservice.domain.token.repository.ReservationTokenRepository
+import org.quartz.Job
+import org.quartz.JobExecutionContext
+import org.slf4j.LoggerFactory
+import org.springframework.data.domain.PageRequest
+import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
+import java.time.LocalDateTime
+
+@Component
+class TokenActivationJob(
+    private val tokenRepository: ReservationTokenRepository,
+) : Job {
+    private val logger = LoggerFactory.getLogger(javaClass)
+
+    @Transactional
+    override fun execute(context: JobExecutionContext) {
+        logger.info("Executing TokenActivationJob at ${LocalDateTime.now()} by instance: ${context.scheduler.schedulerInstanceId}")
+        activateTokens(LocalDateTime.now())
+    }
+
+    fun activateTokens(currentTime: LocalDateTime) {
+        val activeCount = tokenRepository.getActiveTokenCount(TokenStatus.ACTIVE)
+        val tokensToActivate = MAX_TOKEN_COUNT - activeCount
+
+        if (tokensToActivate > 0) {
+            val waitingTokens =
+                tokenRepository.getWaitingTokensForActivation(
+                    TokenStatus.WAITING,
+                    PageRequest.of(0, tokensToActivate),
+                )
+
+            val idsToActivate = waitingTokens.map { it.id }
+            tokenRepository.updateTokenStatus(idsToActivate, TokenStatus.ACTIVE)
+        }
+    }
+}
