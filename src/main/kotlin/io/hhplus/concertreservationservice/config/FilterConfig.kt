1479f24b8b8f6818bd49481e35e127c2f1c88830
ydj515 <ydj515@hanmail.net>
2025-01-10 04:37:00 +0900

    feat: filter, swagger config 추가

---
+++ src/main/kotlin/io/hhplus/concertreservationservice/config/FilterConfig.kt
@@ -0,0 +1,20 @@
+package io.hhplus.concertreservationservice.config
+
+import io.hhplus.concertreservationservice.infrastructure.filter.JwtAuthenticationFilter
+import org.springframework.boot.web.servlet.FilterRegistrationBean
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class FilterConfig {
+    @Bean
+    fun jwtAuthenticationFilterRegistration(
+        jwtAuthenticationFilter: JwtAuthenticationFilter,
+    ): FilterRegistrationBean<JwtAuthenticationFilter> {
+        val registrationBean = FilterRegistrationBean(jwtAuthenticationFilter)
+        registrationBean.addUrlPatterns("/api/*")
+        registrationBean.addUrlPatterns("/reservation-token/status")
+        registrationBean.addInitParameter("excludePaths", "/reservation-token")
+        return registrationBean
+    }
+}
