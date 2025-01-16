dbde54ac8defac3dd165d8d23e6b68f3dd9608b5
ydj515 <ydj515@hanmail.net>
2025-01-17 03:14:19 +0900

    refactor: api 공통 응답 적용

---
+++ src/main/kotlin/io/hhplus/concertreservationservice/presentation/advice/GlobalResponseAdvice.kt
@@ -0,0 +1,62 @@
+package io.hhplus.concertreservationservice.presentation.advice
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import io.hhplus.concertreservationservice.common.response.ApiResponse
+import org.springframework.core.MethodParameter
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageConverter
+import org.springframework.http.server.ServerHttpRequest
+import org.springframework.http.server.ServerHttpResponse
+import org.springframework.lang.Nullable
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.context.request.RequestContextHolder
+import org.springframework.web.context.request.ServletRequestAttributes
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice
+import java.lang.reflect.ParameterizedType
+
+@RestControllerAdvice
+class GlobalResponseAdvice(private val objectMapper: ObjectMapper) : ResponseBodyAdvice<Any> {
+    // controller의 매핑 반환 형식
+    override fun supports(
+        returnType: MethodParameter,
+        converterType: Class<out HttpMessageConverter<*>>,
+    ): Boolean {
+        // Swagger 경로 제외
+        val request = (RequestContextHolder.getRequestAttributes() as ServletRequestAttributes).request
+        val path = request.requestURI
+
+        // Swagger 경로가 아닌 경우만 처리
+        if (path.startsWith("/v3/api-docs") || path.startsWith("/swagger-ui")) {
+            return false
+        }
+
+        var type: Class<*> = returnType.parameterType
+        if (ResponseEntity::class.java.isAssignableFrom(type)) {
+            try {
+                val parameterizedType = returnType.genericParameterType as ParameterizedType
+                type = parameterizedType.actualTypeArguments[0] as Class<*>
+            } catch (ex: ClassCastException) {
+                return false
+            } catch (ex: ArrayIndexOutOfBoundsException) {
+                return false
+            }
+        }
+        return !ApiResponse::class.java.isAssignableFrom(type)
+    }
+
+    // body는 실제로 직렬화할 객체
+    override fun beforeBodyWrite(
+        @Nullable body: Any?,
+        returnType: MethodParameter,
+        selectedContentType: MediaType,
+        selectedConverterType: Class<out HttpMessageConverter<*>>,
+        request: ServerHttpRequest,
+        response: ServerHttpResponse,
+    ): Any? {
+        return when (body) {
+            is ResponseEntity<*> -> body // ResponseEntity 는 그대로 반환
+            else -> ApiResponse.success(body) // 그 외 응답을 SuccessResponse 로 변환
+        }
+    }
+}
