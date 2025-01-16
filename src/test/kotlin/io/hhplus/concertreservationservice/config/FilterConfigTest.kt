93e143971c5b154085f63c23ea4031ae265a3df2
ydj515 <ydj515@hanmail.net>
2025-01-17 03:35:49 +0900

    test: filter config test 추가

---
+++ src/test/kotlin/io/hhplus/concertreservationservice/config/FilterConfigTest.kt
@@ -0,0 +1,28 @@
+package io.hhplus.concertreservationservice.config
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.collections.shouldContain
+import io.kotest.matchers.shouldBe
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.web.servlet.FilterRegistrationBean
+import org.springframework.context.ApplicationContext
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class FilterConfigTest(
+    private val applicationContext: ApplicationContext,
+) : StringSpec({
+
+        "필터가 잘 등록되었는지 확인" {
+            val filterBean = applicationContext.getBean(FilterRegistrationBean::class.java)
+
+            val urlPatterns = filterBean.urlPatterns
+            urlPatterns shouldContain "/api/*"
+            urlPatterns shouldContain "/reservation-token/status"
+
+            // 필터 제외 경로 확인
+            val excludePaths = filterBean.initParameters["excludePaths"]
+            excludePaths shouldBe "/reservation-token"
+        }
+    })
