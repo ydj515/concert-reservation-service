c93bbe1defb458c937dfdb36fc81bb662babac38
ydj515 <ydj515@hanmail.net>
2025-01-17 03:07:08 +0900

    feat: api 요청응답 로깅을 위한 interceptor 추가

---
+++ src/main/kotlin/io/hhplus/concertreservationservice/infrastructure/interceptor/RequestLog.kt
@@ -0,0 +1,25 @@
+package io.hhplus.concertreservationservice.infrastructure.interceptor
+
+import jakarta.servlet.http.HttpServletRequest
+
+data class RequestLog(
+    val url: String,
+    val method: String,
+    val headers: String,
+    val queryParams: String,
+) {
+    override fun toString(): String {
+        return "RequestLog(url='$url', method='$method', headers='$headers', queryParams='$queryParams')"
+    }
+}
+
+fun createRequestLog(request: HttpServletRequest): RequestLog {
+    val headers = request.headerNames.toList().joinToString(", ")
+    val queryParams = request.parameterMap.entries.joinToString(", ") { "${it.key}=${it.value.joinToString(",")}" }
+    return RequestLog(
+        url = request.requestURL.toString(),
+        method = request.method,
+        headers = headers,
+        queryParams = queryParams,
+    )
+}
