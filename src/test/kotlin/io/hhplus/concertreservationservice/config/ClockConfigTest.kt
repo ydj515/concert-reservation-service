ca6acdd268d0d53e5bbd0a8037e0ac0719119a1e
ydj515 <ydj515@hanmail.net>
2025-02-07 02:07:00 +0900

    fix: clock timezone을 UTC로 변경

--- src/test/kotlin/io/hhplus/concertreservationservice/config/ClockConfigTest.kt
+++ src/test/kotlin/io/hhplus/concertreservationservice/config/ClockConfigTest.kt
@@ -4,7 +4,7 @@ import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import org.springframework.context.annotation.Import
 import java.time.Clock
-import java.time.ZoneId
+import java.time.ZoneOffset
 
 @Import(ClockConfig::class)
 class ClockConfigTest(
@@ -12,8 +12,8 @@ class ClockConfigTest(
 ) : FunSpec({
         test("clock 설정을 테스트 한다.") {
             val currentInstant = clock.instant()
-            val zonedDateTime = currentInstant.atZone(ZoneId.of("Asia/Seoul"))
+            val zonedDateTime = currentInstant.atZone(ZoneOffset.UTC)
 
-            zonedDateTime.zone.id shouldBe "Asia/Seoul" // 테스트 확인
+            zonedDateTime.zone.id shouldBe "Z" // 테스트 확인
         }
     })
