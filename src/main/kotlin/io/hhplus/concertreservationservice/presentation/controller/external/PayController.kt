b77f08dabdec810128fc99f3d91a53d24965aaac
ydj515 <ydj515@hanmail.net>
2025-02-20 01:16:34 +0900

    fix: 불필요 print문 logger로 변경

--- src/main/kotlin/io/hhplus/concertreservationservice/presentation/controller/external/PayController.kt
+++ src/main/kotlin/io/hhplus/concertreservationservice/presentation/controller/external/PayController.kt
@@ -1,6 +1,7 @@
 package io.hhplus.concertreservationservice.presentation.controller.external
 
 import io.hhplus.concertreservationservice.application.client.ExternalPayCommand
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -9,10 +10,12 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/api/pay")
 class PayController {
+    private val logger = LoggerFactory.getLogger(javaClass)
+
     @PostMapping("/send")
     fun send(
         @RequestBody externalPayCommand: ExternalPayCommand,
     ) {
-        println(externalPayCommand)
+        logger.info("Sending external pay : {}", externalPayCommand)
     }
 }
